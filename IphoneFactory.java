f4350f4397ad3e42ed63baa7ec056ff28d823a1b
Matheus Souza <matheuslssouza@usp.br>
2023-01-29 14:10:37 -0300

    corrigindo-warnings

--- IphoneFactory.java
+++ IphoneFactory.java
@@ -25,7 +25,7 @@ public class IphoneFactory {
             break;
 
         }
-
+scanner.close();
       
 
     }
--- TypeIphoneEnum.java
+++ TypeIphoneEnum.java
@@ -1,14 +1,6 @@
 
 public enum TypeIphoneEnum {
 
-    IPHONE_X(1), IPHONE_9(2), IPHONE_13_MINI(3);
-
-    private final Integer numero;
-
-    
-    TypeIphoneEnum(int opcao) {
-        this.numero = opcao;
-    }
-
+    IPHONE_X, IPHONE_9, IPHONE_13_MINI;
 
 }
